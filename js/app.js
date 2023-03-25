a4b1fd91c406723347f5f6cf0afe4e193316119f
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2023-03-25 16:04:45 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,19 +1,6 @@
 (() => {
     "use strict";
     const modules_flsModules = {};
-    function isWebp() {
-        function testWebP(callback) {
-            let webP = new Image;
-            webP.onload = webP.onerror = function() {
-                callback(2 == webP.height);
-            };
-            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-        }
-        testWebP((function(support) {
-            let className = true === support ? "webp" : "no-webp";
-            document.documentElement.classList.add(className);
-        }));
-    }
     function functions_getHash() {
         if (location.hash) return location.hash.replace("#", "");
     }
@@ -4610,7 +4597,6 @@
         }
     }));
     window["FLS"] = false;
-    isWebp();
     spollers();
     tabs();
 })();
